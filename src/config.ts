aa9ac298d44c2bb39ab6f10aafb00640b1f3240b
rayanabdirahman <rayanmabdirahman@gmail.com>
2021-11-01 23:22:51 +0000

    add linting and custom logger

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+const config = {
+  APP_PORT: process.env.APP_PORT,
+  APP_DB_NAME: process.env.APP_DB_NAME,
+  APP_DB_USERNAME: process.env.APP_DB_USERNAME,
+  APP_DB_PASSWORD: process.env.APP_DB_PASSWORD,
+  API_URL: process.env.API_URL,
+  APP_JWT_SECRET: process.env.APP_JWT_SECRET,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("starting app");
+// config environment variables
+import dotenv from 'dotenv';
+dotenv.config();
+import config from './config';
+import bootstrapApp from './app';
+import logger from './utilities/logger';
+
+const runApp = async () => {
+  try {
+    logger.debug(`[START]: Bootstrapping app`);
+    const PORT = process.env.PORT || config.APP_PORT;
+
+    const app = await bootstrapApp();
+
+    app.listen(PORT, () => logger.info(`App running on PORT: ${PORT}`));
+
+    return app;
+  } catch (error) {
+    logger.error(`Failed to run app: ${error}`);
+  }
+};
+
+(async () => await runApp())();
