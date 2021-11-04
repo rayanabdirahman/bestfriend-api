2a87afbdae47e6787d120242e98c8e1f9aa02fb6
rayanabdirahman <rayanmabdirahman@gmail.com>
2021-11-04 01:44:13 +0000

    add AdminGuard middleware

---
+++ src/middlewares/AdminGuard.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import ApiResponse from '../utilities/api-response';
+import logger from '../utilities/logger';
+
+const AdminGuard = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { user } = req;
+    if (!user.isAdmin) {
+      throw new Error('You do not have the admin right to perform this action');
+    }
+    next();
+  } catch (error: any) {
+    const message = error.message ? error.message : error;
+    logger.error(
+      `[AdminGuard] - Unable to authorise user as admin: ${message}`
+    );
+    return ApiResponse.error(res, message);
+  }
+};
+
+export default AdminGuard;
